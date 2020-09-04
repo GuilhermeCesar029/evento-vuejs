679e868354fda1fc976eb0878c3920f07655e2f3
GuilhermeCesar029 <guicesar029@gmail.com>
2020-09-04 14:37:42 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Evento/>
+  </div>
+</template>
+
+<script>
+import Evento from './components/Evento'
+
+export default {
+  name: 'App',
+  components: {
+    Evento
+  }
+}
+</script>
---
+++ src/components/Evento.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="evento-1">
+        <button v-on:click="cont++">Adiciona 1</button>
+        <p>Quantas vezes o botão foi clicado: {{cont}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                cont: 0
+            }
+        }
+    }
+</script>
